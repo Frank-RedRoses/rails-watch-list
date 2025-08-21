9ba65a962bfb4ac7f8353409ec2c145238fd6c67
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-08-21 21:34:07 +0200

    add movie simple validation to check that movie has no bookmark before deleting

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,13 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
+  before_destroy :check_has_no_bookmark
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
+
+  private
+
+  def check_has_no_bookmark
+    throw :abort unless this.bookmarks.empty?
+  end
 end
