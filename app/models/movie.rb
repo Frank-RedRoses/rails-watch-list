60f9cf7883ebe40f1c43b152953cda787526e99e
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-08-21 21:39:48 +0200

    Revert "add movie simple validation to check that movie has no bookmark before deleting"
    This reverts commit 9ba65a962bfb4ac7f8353409ec2c145238fd6c67.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,6 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  before_destroy :check_has_no_bookmark
 
   validates :title, presence: true, uniqueness: true
   validates :overview, presence: true
-
-  private
-
-  def check_has_no_bookmark
-    throw :abort unless this.bookmarks.empty?
-  end
 end
