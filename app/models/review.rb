ebb8d74e537c354974f3589abb05eccdb2b304b1
Franklin Rosas <113691934+Frank-RedRoses@users.noreply.github.com>
2025-08-22 02:49:13 +0200

    Add validations to the Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :list
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
